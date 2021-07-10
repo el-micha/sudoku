0a74583e00183b68b7869f2bd6e920b9558c6e05
Michael Weigelt <michael.weigelt@bluewin.ch>
2021-07-10 11:14:44 +0200

    synch

--- sudoku.hs
+++ sudoku.hs
@@ -15,26 +15,37 @@
 
 import qualified Data.Vector as V
 
+data Sudoku = Sudoku (V.Vector Int)
 
-emptyLine :: V.Vector Int
-emptyLine = V.replicate 10 0
-
-type Sudoku = V.Vector (V.Vector Int)
+instance Show Sudoku where
+  show (Sudoku v) = undefined
 
 emptySudoku :: Sudoku
-emptySudoku = V.replicate 10 emptyLine
+emptySudoku = Sudoku $ V.replicate 81 0
 
-get :: Sudoku -> Int -> Int -> Int
-get s x y = (s V.! x) V.! y
+rows :: Sudoku -> [V.Vector Int]
+rows (Sudoku s) = [V.slice (start * 9) 9 s | start <- [0..8]]
+-- slice 0,9, slice 9,9, slice 18,9 etc 
 
-put :: Sudoku -> Int -> Int -> Int -> Sudoku
-put s x y n = s V.// [(x, (s V.! x) V.// [(y, n)])]
+--emptyLine :: V.Vector Int
+--emptyLine = V.replicate 10 0
 
-getRow :: Sudoku -> Int -> [Int]
-getRow s n = V.toList $ s V.! n
+--type Sudoku = V.Vector (V.Vector Int)
+
+--emptySudoku :: Sudoku
+--emptySudoku = V.replicate 10 emptyLine
+
+--get :: Sudoku -> Int -> Int -> Int
+--get s x y = (s V.! x) V.! y
+
+--put :: Sudoku -> Int -> Int -> Int -> Sudoku
+--put s x y n = s V.// [(x, (s V.! x) V.// [(y, n)])]
+
+--getRow :: Sudoku -> Int -> [Int]
+--getRow s n = V.toList $ s V.! n
  
-getCol :: Sudoku -> Int -> [Int]
-getCol s n = [row V.! n | row <- V.toList s]
+--getCol :: Sudoku -> Int -> [Int]
+--getCol s n = [row V.! n | row <- V.toList s]
  
 
 
